7ff175269a2cf2d427c933b5345c1db0323c0115
Sydney Cardew <sydney.cardew@gmail.com>
2024-10-07 17:42:28 +0100

    bugfix for linux

--- write_functions.py
+++ write_functions.py
@@ -5,14 +5,14 @@ from datetime import date
 
 
 def write_txt(table_str, config):
-    output_path = os.getcwd() + config.output_path
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
+    func_output_path = os.getcwd() + config.output_path
+    if not os.path.exists(func_output_path):
+        os.mkdir(func_output_path)
     name = f"{config.output_name} from {config.target} on {date.today()}"
     increment = 1
-    while os.path.exists(f"{output_path}{name} - {str(increment).zfill(3)}.txt"):
+    while os.path.exists(f"{func_output_path}{name} - {str(increment).zfill(3)}.txt"):
         increment = increment + 1
-    with open(f'{config.output_path}{name} - {str(increment).zfill(3)}.txt', "w+") as write_file:
+    with open(f'{func_output_path}{name} - {str(increment).zfill(3)}.txt', "w+") as write_file:
         write_file.write(table_str)
 
 
